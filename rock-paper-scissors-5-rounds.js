77efc7fe2e196bc92124c4779d7370fed1e16ff6
John Macharia <machariajohn23@gmail.com>
2022-10-15 20:33:27 +0300

    Play 'Rock Paper Scissors' 5 times, without keeping tallies and scores

---
+++ rock-paper-scissors-5-rounds.js
@@ -0,0 +1,71 @@
+//'use strict';
+
+
+function getComputerChoice() {
+    const myArray = ['Rock', 'Paper', 'Scissors'];
+    const randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+    return randomItem.toLowerCase();
+};
+
+//let playerScore = 0;
+//let computerScore = 0;
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    result = "Draw";
+    return result;
+  }
+
+  else if (playerSelection == "rock" && computerSelection == "scissors") {
+    result = "You Win! Rock beats Scissors";
+    return result;
+  }
+
+  else if (playerSelection == "scissors" && computerSelection == "paper") {
+    result = "You Win! Scissors beats Paper ";
+    return result;
+  }
+
+  else if (playerSelection == "paper" && computerSelection == "rock") {
+    result = "You Win! Paper beats Rock ";
+    return result;
+  }
+
+  else if (playerSelection == "scissors" && computerSelection == "rock") {
+    result = "You Lose! Rock beats Scissors";
+    return result;
+  }
+
+  else if (playerSelection == "paper" && computerSelection == "scissors") {
+    result = "You Lose! Scissors beats Paper";
+    return result;
+  }
+
+  else if (playerSelection == "rock" && computerSelection == "paper") {
+    result = "You Lose! Paper beats Rock";
+    return result;
+  }
+}
+
+
+//const playerSelection = "scissors";
+//console.log(`player: ${playerSelection}`);
+//const computerSelection = getComputerChoice();
+//console.log(`computer: ${computerSelection}`);
+//console.log(playRound(playerSelection, computerSelection));
+
+
+// Function game() plays 'Rock Paper Scissors for five rounds
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Choose between 'rock', 'paper' and 'scissors'");
+    console.log(`player: ${playerSelection}`);
+    const computerSelection = getComputerChoice();
+    console.log(`computer: ${computerSelection}`);
+    console.log(playRound(playerSelection, computerSelection));
+
+  }
+}
+
+console.log(game());
