5cbb992c087f7df690ff727a343256f1004820af
John Macharia <machariajohn23@gmail.com>
2022-10-15 21:26:21 +0300

    Rock paper Scissors with Results tally and Final score

--- rock-paper-scissors-5-rounds.js
+++ rock-paper-scissors-5-rounds.js
@@ -7,42 +7,55 @@ function getComputerChoice() {
     return randomItem.toLowerCase();
 };
 
-//let playerScore = 0;
-//let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
     result = "Draw";
+    console.log(`player: ${playerScore} - computer: ${computerScore}`);
     return result;
   }
 
   else if (playerSelection == "rock" && computerSelection == "scissors") {
     result = "You Win! Rock beats Scissors";
+    playerScore++;
+    console.log(`player: ${playerScore} - computer: ${computerScore}`);
     return result;
   }
 
   else if (playerSelection == "scissors" && computerSelection == "paper") {
     result = "You Win! Scissors beats Paper ";
+    playerScore++;
+    console.log(`player: ${playerScore} - computer: ${computerScore}`);
     return result;
   }
 
   else if (playerSelection == "paper" && computerSelection == "rock") {
     result = "You Win! Paper beats Rock ";
+    playerScore++;
+    console.log(`player: ${playerScore} - computer: ${computerScore}`);
     return result;
   }
 
   else if (playerSelection == "scissors" && computerSelection == "rock") {
     result = "You Lose! Rock beats Scissors";
+    computerScore++;
+    console.log(`player: ${playerScore} - computer: ${computerScore}`);
     return result;
   }
 
   else if (playerSelection == "paper" && computerSelection == "scissors") {
     result = "You Lose! Scissors beats Paper";
+    computerScore++;
+    console.log(`player: ${playerScore} - computer: ${computerScore}`);
     return result;
   }
 
   else if (playerSelection == "rock" && computerSelection == "paper") {
     result = "You Lose! Paper beats Rock";
+    computerScore++;
+    console.log(`player: ${playerScore} - computer: ${computerScore}`);
     return result;
   }
 }
@@ -65,6 +78,13 @@ function game() {
     console.log(`computer: ${computerSelection}`);
     console.log(playRound(playerSelection, computerSelection));
 
+  }
+  if (playerScore > computerScore) {
+    return "FINAL SCORE: Player Wins";
+  } else if (computerScore == playerScore) {
+    return "FINAL SCORE: Draw";
+  } else {
+    return "FINAL SCORE: Computer Wins"
   }
 }
 
